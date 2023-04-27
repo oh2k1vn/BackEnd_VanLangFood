eaae8d106ef5264f4c193e9ffdc5ccc58ec7dd0d
nconghau <hau2803nch@gmail.com>
2023-04-27 17:30:39 +0700

    first commit

---
+++ products.js
@@ -0,0 +1,31 @@
+const products = [
+  {
+    id: 1,
+    name: "Trà Sữa",
+    brand: "Drink",
+    desc: "Trà Sữa",
+    price: 20000,
+    image:
+      "https://res.cloudinary.com/dcnzwz9sp/image/upload/v1682565574/aya-tra-dau-thanh-pho-ha-long-quang-ninh-1655515308526638077-eatery-avatar-1655543074_fd86ki.png",
+  },
+  {
+    id: 2,
+    name: "Trà Dâu",
+    brand: "Drink",
+    desc: "Trà Dâu",
+    price: 20000,
+    image:
+      "https://res.cloudinary.com/dcnzwz9sp/image/upload/v1682565574/aya-tra-dau-thanh-pho-ha-long-quang-ninh-1655515308526638077-eatery-avatar-1655543074_fd86ki.png",
+  },
+  {
+    id: 3,
+    name: "Cơm gà",
+    brand: "Rice",
+    desc: "Cơm gà",
+    price: 25000,
+    image:
+      "https://res.cloudinary.com/dcnzwz9sp/image/upload/v1682565574/aya-tra-dau-thanh-pho-ha-long-quang-ninh-1655515308526638077-eatery-avatar-1655543074_fd86ki.png",
+  },
+];
+
+module.exports = products;
